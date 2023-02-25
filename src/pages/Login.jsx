c18dd2b384ef9e16d0ad92dedb1aa721052013c6
dboatengg <dicksonboateng67@gmail.com>
2023-02-25 22:39:30 +0000

    Refactor and style login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,6 +3,8 @@ import { auth, provider } from "../firebase";
 import { signInWithPopup } from "firebase/auth";
 import { useNavigate } from "react-router-dom";
 
+import { FcGoogle } from "react-icons/fc";
+
 const Login = ({ setIsAuth }) => {
   let navigate = useNavigate();
 
@@ -10,21 +12,23 @@ const Login = ({ setIsAuth }) => {
     signInWithPopup(auth, provider).then((result) => {
       localStorage.setItem("isAuth", true);
       setIsAuth(true);
-      navigate("/");
+      navigate("/blog");
     });
   };
 
   const style = {
-    container: `h-[90vh] flex items-center justify-center`,
+    container: `h-[90vh] flex items-center justify-center w-[90%] mx-auto`,
     content: `flex flex-col gap-5 items-center`,
-    button: `bg-red-400 px-4 py-2 shadow-md`,
-    title: `text-4xl max-w-[500px] mx-auto text-center w-[90%]`,
+    button: `bg-green-200 transition duration-300 ease-in-out px-6 py-3 shadow-lg flex items-center gap-2 sm:px-4 sm:py-2 hover:shadow-md hover:bg-green-300`,
+    title: `text-4xl max-w-[500px] mx-auto text-center w-[90%] sm:text-2xl`,
+    googleIcon: `text-3xl sm:text-2xl`,
   };
   return (
     <div className={style.container}>
       <div className={style.content}>
         <h2 className={style.title}>Sign In With Google to continue</h2>
         <button className={style.button} onClick={signInWithGoogle}>
+          <FcGoogle className={style.googleIcon} />
           Sign in with Google
         </button>
       </div>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -2,6 +2,10 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    screens: {
+      sm: { max: "500px" },
+      md: { max: "700px" },
+    },
     extend: {},
   },
   plugins: [],

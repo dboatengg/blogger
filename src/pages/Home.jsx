a4414aa1ac79f3dc84131a806341a6c7d81c26e2
dboatengg <dicksonboateng67@gmail.com>
2023-02-25 22:44:57 +0000

    Refactor: Update styles of homepage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 
 const Home = () => {
   const style = {
-    container: `h-[90vh] flex items-center justify-center`,
-    title: `text-4xl`,
+    container: `h-[90vh] flex items-center justify-center flex-col gap-5 max-w-[400px] w-[90%] mx-auto text-center`,
+    title: `text-4xl sm:text-3xl`,
   };
   return (
     <div className={style.container}>
-      <h1 className={style.title}>Welcome Home</h1>
+      <h1 className={style.title}>Welcome To Blogger!</h1>
+      <p>
+        Blogger is a platform that allows you create, view, and delete blog
+        posts.
+      </p>
     </div>
   );
 };
